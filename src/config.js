652854cf1209923c0c12fbc359f4363fcea247ef
Anuj <Anuj@C40DESK-10090.india.rsystems.com>
2022-11-05 20:22:49 +0530

    cal-node-sql-01-managing-node-app - defined config and env variables.

---
+++ src/config.js
@@ -0,0 +1,46 @@
+"use strict";
+
+require("dotenv").config();
+const assert = require("assert");
+
+const {
+  PORT,
+  HOST,
+  HOST_URL,
+  COOKIE_ENCRYPT_PWD,
+  SQL_USER,
+  SQL_PASSWORD,
+  SQL_DATABASE,
+  SQL_SERVER,
+  SQL_ENCRYPT,
+  OKTA_ORG_URL,
+  OKTA_CLIENT_ID,
+  OKTA_CLIENT_SECRET
+} = process.env;
+
+const sqlEncrypt = process.env.SQL_ENCRYPT === true;
+
+assert(PORT, "Port is required.");
+assert(HOST, "Host is required.");
+
+module.exports = {
+  port: PORT,
+  host: HOST,
+  url: HOST_URL,
+  cookiePwd: COOKIE_ENCRYPT_PWD,
+  sql: {
+    server: SQL_SERVER,
+    database: SQL_DATABASE,
+    user: SQL_USER,
+    password: SQL_PASSWORD,
+    options: {
+      encrypt: SQL_ENCRYPT,
+      enableArithAbort: true
+    }
+  },
+  okta: {
+    url: OKTA_ORG_URL,
+    clientId: OKTA_CLIENT_ID,
+    clientSecret: OKTA_CLIENT_SECRET
+  }
+};
